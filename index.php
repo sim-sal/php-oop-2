bfbf5c851f05754d33345d4aadfe1595e292ec50
sim-sal <simone.saladino93@gmail.com>
2023-07-03 20:26:53 +0200

    added Exception in variable "prezzo"

--- index.php
+++ index.php
@@ -5,15 +5,20 @@ require_once("./models/Categoria.php");
 require_once("./models/ProdottoCane.php");
 require_once("./models/ProdottoGatto.php");
 
-// aggiungiamo le due categorie nella classe Categoria
-$categoriaCane = new Categoria("Cane");
-$categoriaGatto = new Categoria("Gatto");
+try {
+    // aggiungiamo le due categorie nella classe Categoria
+    $categoriaCane = new Categoria("Cane");
+    $categoriaGatto = new Categoria("Gatto");
 
+    // creo un prodotto per il cane
+    $prodottoCane1 = new ProdottoCane("osso", "ossoImg", 10, $categoriaCane, "giochi");
+    var_dump($prodottoCane1);
 
-$prodottoCane1 = new ProdottoCane("osso", "ossoImg", 10, $categoriaCane, "giochi");
-var_dump($prodottoCane1);
+    echo "<br><br>";
 
-echo "<br><br>";
-
-$prodottoGatto1 = new ProdottoGatto("collare", "collareImg", 5, $categoriaGatto, "accessori");
-var_dump($prodottoGatto1);
+    // creo un prodotto per il gatto
+    $prodottoGatto1 = new ProdottoGatto("collare", "collareImg", 5, $categoriaGatto, "accessori");
+    var_dump($prodottoGatto1);
+} catch (Exception $e) {
+    echo "Errore nel prezzo: " . $e->getMessage();
+}
