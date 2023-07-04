94975ddbbbe13aa9f07523e45f5906a8a62bb36c
sim-sal <simone.saladino93@gmail.com>
2023-07-04 19:17:36 +0200

    added comments

--- data/db.php
+++ data/db.php
@@ -11,15 +11,21 @@ try {
     $categoriaCane = new Categoria('<i class="fa-solid fa-dog"></i>');
     $categoriaGatto = new Categoria('<i class="fa-solid fa-cat"></i>');
 
+    // PRODOTTI PER IL CANE
     // creo un prodotto cibo per il cane
     $ciboCane1 = new Cibo("Cesar 'Pollo&Manzo'", "https://m.media-amazon.com/images/I/71nSzTZ+kHL._AC_SL1500_.jpg", 13.86, $categoriaCane, "Cibo");
+    // creo un prodotto accessorio per il cane
     $accessorioCane1 = new Accessorio("Lava zampe", "https://m.media-amazon.com/images/I/81RghwxB42L._AC_SL1500_.jpg", 14.99, $categoriaCane, "Accessori");
+    // creo un prodotto gioco per il cane
     $giocoCane1 = new Gioco("Tiro alla fune", "https://m.media-amazon.com/images/I/812HiDaUXYL._AC_SL1500_.jpg", 11.99, $categoriaCane, "Giochi");
 
 
-    // creo un prodotto per il gatto
-    $accessorioGatto1 = new Accessorio("Lettiera", "https://m.media-amazon.com/images/I/71h5k6tT2qL._AC_SL1500_.jpg", 24.99, $categoriaGatto, "Accessori");
+    // PRODOTTI PER IL GATTO
+    // creo un prodotto cibo per il gatto
     $ciboGatto1 = new Cibo("Purina Gourmet", "https://m.media-amazon.com/images/I/71gabeunxzL._AC_SL1500_.jpg", 17.49, $categoriaGatto, "Cibo Umido");
+    // creo un prodotto accessorio per il gatto
+    $accessorioGatto1 = new Accessorio("Lettiera", "https://m.media-amazon.com/images/I/71h5k6tT2qL._AC_SL1500_.jpg", 24.99, $categoriaGatto, "Accessori");
+    // creo un prodotto gioco per il gatto
     $giocoGatto1 = new Gioco("Palla Interattiva", "https://m.media-amazon.com/images/I/61nbqP3EE9L._AC_SL1500_.jpg", 11.69, $categoriaGatto, "Giochi");
 
     // inseriamo i prodotti di entrambe le categorie in un unico array
